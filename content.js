06d99615d68510208cfdd6fdd9fa41579a5b1fe6
abhinavsaraswatt <abhinavsaraswat2411@gmail.com>
2022-11-01 22:23:56 +0530

    taking color from user input

--- content.js
+++ content.js
@@ -1,2 +1,10 @@
 // Send the message to eventpage to highlight the extension on particular domain.
 chrome.runtime.sendMessage({ todo: "showPageAction" });
+
+// Receiving message to change page color
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.todo == "changeColor") {
+    var addColor = "#" + request.clickedColor;
+    $(".stack").css("color", addColor);
+  }
+});
